80c71653c62a78f62c8df41aef3ff76017729f9a
Connor Kasarda <connorkasarda@gmail.com>
2022-09-27 15:41:50 -0600

    Removed pygame, created animation of spinning triangle with gradient of colors.

--- tests/graphics.py
+++ tests/graphics.py
@@ -1,67 +1,37 @@
-import pygame as pg
-from OpenGL.GL import *
 import numpy as np
-import ctypes
-from OpenGL.GL.shaders import compileProgram, compileShader
+from OpenGL.GL import *
+import glfw
 
-class Graphics:
-    def __init__(self):
-        pg.init()
-        pg.display.set_mode((640, 480), pg.OPENGL | pg.DOUBLEBUF)
-        self.clock = pg.time.Clock()
-        glClearColor(0.1, 0.2, 0.2, 1)
-        self.shader = self.create_shader("tests/shaders/vertex.txt", "tests/shaders/fragment.txt")
-        glUseProgram(self.shader)
-        self.triangle = Triangle()
-        self.loop()
-    def create_shader(self, vertex_filepath, fragment_filepath):
-        with open(vertex_filepath, 'r') as f:
-            vertex_src = f.readlines()
-        with open(fragment_filepath, 'r') as f:
-            fragment_src = f.readlines()
-        shader = compileProgram(
-            compileShader(vertex_src, GL_VERTEX_SHADER),
-            compileShader(fragment_src, GL_FRAGMENT_SHADER)
-        )
-        return shader
-    def loop(self):
-        running = True
-        while (running):
-            for event in pg.event.get():
-                if (event.type == pg.QUIT):
-                    running = False
-            glClear(GL_COLOR_BUFFER_BIT)
-            glUseProgram(self.shader)
-            glBindVertexArray(self.triangle.vao)
-            glDrawArrays(GL_TRIANGLES, 0, self.triangle.vertex_count)
-            pg.display.flip()
-            self.clock.tick(60)
-        self.quit()
-    def quit(self):
-        self.triangle.destroy()
-        glDeleteProgram(self.shader)
-        pg.quit()
+glfw.init()
+window = glfw.create_window(900, 700, "PyOpenGL Triangle", None,None)
+glfw.set_window_pos(window, 600, 300)
+glfw.make_context_current(window)
 
-class Triangle:
-    def __init__(self):
-        self.vertices = (
-            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-             0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-             0.0,  0.5, 0.0, 0.0, 0.0, 1.0
-        )
-        self.vertices = np.array(self.vertices, dtype=np.float32)
-        self.vertex_count = 3
-        self.vao = glGenVertexArrays(1)
-        glBindVertexArray(self.vao)
-        self.vbo = glGenBuffers(1)
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
-        glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
-    def destroy(self):
-        glDeleteVertexArrays(1, (self.vao, ))
-        glDeleteBuffers(1, (self.vbo, ))
+vertices = [-0.2, -0.2, 0.0,
+             0.2, -0.2, 0.0,
+             0.0,  0.2, 0.0]
 
-if __name__ == "__main__":
-    graphics = Graphics()
+# list the color code here
+colors = [0.5, 0.5, 0,
+          0,   0.8, 0.9,
+          0,   0.2, 1.0 ]
+
+v = np.array (vertices, dtype = np.float32)
+c = np.array (colors, dtype = np.float32)
+# this will create a color-less triangle
+glEnableClientState(GL_VERTEX_ARRAY)
+glVertexPointer(3, GL_FLOAT, 0, v)
+glEnableClientState(GL_COLOR_ARRAY)
+glColorPointer(3, GL_FLOAT, 0, c)
+# Now we will pour color for the animation's background
+glClearColor(0.6, 0.8, 0.2, 0.2)
+
+while not glfw.window_should_close(window):
+# this while loop will keep iterating all the functions below until the window is not closed
+    glfw.poll_events()
+    glClear(GL_COLOR_BUFFER_BIT)
+# creating rotation animated motion
+    glRotatef(0.1, 1, 0, 1)
+    glDrawArrays(GL_TRIANGLES, 0, 3)
+    glfw.swap_buffers(window)
+glfw.terminate()
