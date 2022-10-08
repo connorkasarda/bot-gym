567d7a5d5c00c9b5eceaf624cfc8bbe2151e6a89
Connor Kasarda <connorkasarda@gmail.com>
2022-10-07 21:27:39 -0600

    Fixed spacing\.

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function animate() {
 
     // Rotate the cube
     cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+    cube.rotation.y += 0.01;
 
     // Render the scene
     renderer.render(scene, camera);
