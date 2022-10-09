b0617cd3fe628ab3b413fb045dfec20cd4a1899d
Connor Kasarda <connorkasarda@gmail.com>
2022-10-09 15:15:38 -0600

    Settled on color theme for now. Updated directory paths. Added header with project name and my name. Made 3D space background clear.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,18 @@
 // Import the THREE javascript file for 3D graphics
-import * as THREE from './scr/three.module.js';
+import * as THREE from './lib/three.module.js';
+import * as AMMO from './lib/ammo.js';
 // Creates the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({alpha: true});
 // Sets the render size and add the renderer to HTML document
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 // Adds yellow cube to the scene
 const cube_geometry = new THREE.BoxGeometry(1, 1, 1);
-const cube_material = new THREE.MeshBasicMaterial({color: 0xFFFF00});
+const cube_material = new THREE.MeshBasicMaterial({color: 0x7A2048});
 const cube = new THREE.Mesh(cube_geometry, cube_material);
 scene.add(cube);
-// Adds static (non-moving) blue lines to the scene
-const line_points = [];
-line_points.push(new THREE.Vector3(-2, 0, 0));
-line_points.push(new THREE.Vector3(0, 2, 0));
-line_points.push(new THREE.Vector3(2, 0, 0));
-line_points.push(new THREE.Vector3(0, -2, 0));
-line_points.push(new THREE.Vector3(-2, 0, 0));
-const line_geometry = new THREE.BufferGeometry().setFromPoints(line_points);
-const line_material = new THREE.LineBasicMaterial({color: 0x0000FF});
-const line = new THREE.Line(line_geometry, line_material);
-line.matrixAutoUpdate = false;
-scene.add(line);
 // Sets the camera position
 camera.position.set(0, 0, 10);
 // Creates animation loop function
