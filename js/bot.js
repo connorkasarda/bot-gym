968fdf79f6f60ad7a4c8a4b8e84a2b9838e15fb8
Connor Kasarda <connorkasarda@gmail.com>
2022-10-11 13:02:06 -0600

    Organizing OOP approach to Robot Gym; added Bot and Gym class. Changed Three.js file import name.

---
+++ js/bot.js
@@ -0,0 +1,6 @@
+// Allows for robots to have 3D appearance
+import * as THREE from './pkg/three.js';
+// Defines a virtual robot
+class Bot {
+    constructor() {}
+}
---
+++ js/gym.js
@@ -0,0 +1,7 @@
+// Allows gym to have a 3D appearance and apply the laws of physics
+import * as THREE from './pkg/three.js';
+import * as AMMO from './pkg/ammo.js';
+// Provides space for robots to be tested in
+class Gym {
+    constructor() {}
+}
